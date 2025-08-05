208eaa95e086138d49809e853c6a49b0c2d8db94
big radish <32863820+phoenix9668@users.noreply.github.com>
2025-08-05 19:33:55 +0800

    在本地运行了wakenet 和 afe，cn_speech_commands 工程，只有 wakenet一直重启，没有运行起来

--- examples/cn_speech_commands_recognition/main/main.c
+++ examples/cn_speech_commands_recognition/main/main.c
@@ -26,7 +26,6 @@ static esp_afe_sr_data_t *afe_data = NULL;
 static volatile int task_flag = 0;
 srmodel_list_t *models = NULL;
 
-
 void feed_Task(void *arg)
 {
     esp_afe_sr_data_t *afe_data = arg;
@@ -37,12 +36,14 @@ void feed_Task(void *arg)
     int16_t *i2s_buff = malloc(audio_chunksize * sizeof(int16_t) * feed_channel);
     assert(i2s_buff);
 
-    while (task_flag) {
+    while (task_flag)
+    {
         esp_get_feed_data(true, i2s_buff, audio_chunksize * sizeof(int16_t) * feed_channel);
 
         afe_handle->feed(afe_data, i2s_buff);
     }
-    if (i2s_buff) {
+    if (i2s_buff)
+    {
         free(i2s_buff);
         i2s_buff = NULL;
     }
@@ -61,46 +62,57 @@ void detect_Task(void *arg)
     int mu_chunksize = multinet->get_samp_chunksize(model_data);
     assert(mu_chunksize == afe_chunksize);
 
-    //print active speech commands
+    // print active speech commands
     multinet->print_active_speech_commands(model_data);
     printf("------------detect start------------\n");
-    while (task_flag) {
-        afe_fetch_result_t* res = afe_handle->fetch(afe_data); 
-        if (!res || res->ret_value == ESP_FAIL) {
+    while (task_flag)
+    {
+        afe_fetch_result_t *res = afe_handle->fetch(afe_data);
+        if (!res || res->ret_value == ESP_FAIL)
+        {
             printf("fetch error!\n");
             break;
         }
 
-        if (res->wakeup_state == WAKENET_DETECTED) {
+        if (res->wakeup_state == WAKENET_DETECTED)
+        {
             printf("WAKEWORD DETECTED\n");
-	        multinet->clean(model_data);
+            multinet->clean(model_data);
         }
 
-        if (res->raw_data_channels == 1 && res->wakeup_state == WAKENET_DETECTED) {
+        if (res->raw_data_channels == 1 && res->wakeup_state == WAKENET_DETECTED)
+        {
             wakeup_flag = 1;
-        } else if (res->raw_data_channels > 1 && res->wakeup_state == WAKENET_CHANNEL_VERIFIED) {
+        }
+        else if (res->raw_data_channels > 1 && res->wakeup_state == WAKENET_CHANNEL_VERIFIED)
+        {
             // For a multi-channel AFE, it is necessary to wait for the channel to be verified.
             printf("AFE_FETCH_CHANNEL_VERIFIED, channel index: %d\n", res->trigger_channel_id);
             wakeup_flag = 1;
         }
 
-        if (wakeup_flag == 1) {
+        if (wakeup_flag == 1)
+        {
             esp_mn_state_t mn_state = multinet->detect(model_data, res->data);
 
-            if (mn_state == ESP_MN_STATE_DETECTING) {
+            if (mn_state == ESP_MN_STATE_DETECTING)
+            {
                 continue;
             }
 
-            if (mn_state == ESP_MN_STATE_DETECTED) {
+            if (mn_state == ESP_MN_STATE_DETECTED)
+            {
                 esp_mn_results_t *mn_result = multinet->get_results(model_data);
-                for (int i = 0; i < mn_result->num; i++) {
-                    printf("TOP %d, command_id: %d, phrase_id: %d, string:%s prob: %f\n", 
-                    i+1, mn_result->command_id[i], mn_result->phrase_id[i], mn_result->string, mn_result->prob[i]);
+                for (int i = 0; i < mn_result->num; i++)
+                {
+                    printf("TOP %d, command_id: %d, phrase_id: %d, string:%s prob: %f\n",
+                           i + 1, mn_result->command_id[i], mn_result->phrase_id[i], mn_result->string, mn_result->prob[i]);
                 }
                 printf("\n-----------listening-----------\n");
             }
 
-            if (mn_state == ESP_MN_STATE_TIMEOUT) {
+            if (mn_state == ESP_MN_STATE_TIMEOUT)
+            {
                 esp_mn_results_t *mn_result = multinet->get_results(model_data);
                 printf("timeout, string:%s\n", mn_result->string);
                 afe_handle->enable_wakenet(afe_data);
@@ -110,7 +122,8 @@ void detect_Task(void *arg)
             }
         }
     }
-    if (model_data) {
+    if (model_data)
+    {
         multinet->destroy(model_data);
         model_data = NULL;
     }
@@ -129,6 +142,6 @@ void app_main()
     afe_config_free(afe_config);
 
     task_flag = 1;
-    xTaskCreatePinnedToCore(&detect_Task, "detect", 8 * 1024, (void*)afe_data, 5, NULL, 1);
-    xTaskCreatePinnedToCore(&feed_Task, "feed", 8 * 1024, (void*)afe_data, 5, NULL, 0);
+    xTaskCreatePinnedToCore(&detect_Task, "detect", 8 * 1024, (void *)afe_data, 5, NULL, 1);
+    xTaskCreatePinnedToCore(&feed_Task, "feed", 8 * 1024, (void *)afe_data, 5, NULL, 0);
 }
